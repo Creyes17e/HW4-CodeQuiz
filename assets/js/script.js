f38120734a95c151c4c4ccd6d572a9ed62e847b9
Carina Reyes <59748133+Creyes17e@users.noreply.github.com>
2020-04-28 22:00:14 -0500

    Created a render question, start quiz and check Ans functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,50 +9,7 @@ const choice2 = document.getElementById("2");
 const choice3 = document.getElementById("3");
 const choice4 = document.getElementById("4");
 
-//Questions
-const questions = [
-  {
-    question: "What is the correct way to call a function?",
-
-    choice1: "function()",
-    choice2: "console.long(function)",
-    choice3: "print.function",
-    choice4: "document.function",
-    correct: "1",
-  },
-  {
-    question: "Var is used to delcare the ___ in Javascript?",
-
-    choice1: "various",
-    choice2: "variable",
-    choice3: "print.option",
-    choice4: "array",
-    correct: "2",
-  },
-];
-
-//Variables
-const lastQ = questions.length - 1;
-let currentQ = 0;
-
-//Render Questions
-function renderQuestion() {
-  let q = questions[currentQ];
-  question.innerHTML = q.question;
-  choice1.innerHTML = q.choice1;
-  choice2.innerHTML = q.choice2;
-}
-//Start Quiz
-startBtn.addEventListener("click", startQuiz);
-
-function startQuiz() {
-  intro.style.display = "none";
-  renderQuestion();
-  quizEl.style.display = "block";
-}
-
 //Start Timer
-
 startBtn.addEventListener("click", startTimer);
 function startTimer() {
   var secondsLeft = 60;
@@ -65,10 +22,93 @@ function startTimer() {
     }
   }, 1000);
 }
-// startBtn.addEventListener("click", startBtn);
-// function startQuiz() {
-//   startBtn.classList.add("hide");
-//   shuffledQuestions = questions.sort(() => Math.random() - 0.5);
-//   currentQuestionIndex = 0;
-//   quizEl.classList.remove("hide");
-// }
+
+//Questions
+const questions = [
+  {
+    question: "What is the correct way to call a function?",
+
+    choice1: "function()",
+    choice2: "console.long(function)",
+    choice3: "print.function",
+    choice4: "document.function",
+    correct: "1",
+  },
+  {
+    question: "Var is used to delcare the ___ in JavaScript?",
+
+    choice1: "various",
+    choice2: "variable",
+    choice3: "print.option",
+    choice4: "array",
+    correct: "2",
+  },
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    choice1: "<javascript>",
+    choice2: "<js>",
+    choice3: "<scripting>",
+    choice4: "<script>",
+    correct: "4",
+  },
+  {
+    question: "Where is the correct place to insert a JavaScript",
+    choice1: "Both the <head> section and the <body> section",
+    choice2: "The <body> section",
+    choice3: "The <head> section",
+    choice4: "The <title> section",
+    correct: "2",
+  },
+  {
+    question: "How do you write 'You are the best!' in an alert box?",
+    choice1: "msgBox('You are the best!');",
+    choice2: "msg('You are the best!');",
+    choice3: "alert('You are the best!')",
+    choice4: "alertBox('You are the best!')",
+    correct: "3",
+  },
+];
+
+//Variables
+const lastQ = questions.length - 1;
+let currentQ = 0;
+
+//Render Questions, the app will render a question and 4 ans choices
+function renderQuestion() {
+  let q = questions[currentQ];
+  question.innerHTML = q.question;
+  choice1.innerHTML = q.choice1;
+  choice2.innerHTML = q.choice2;
+  choice3.innerHTML = q.choice3;
+  choice4.innerHTML = q.choice4;
+}
+console.log(questions);
+//Start Quiz
+startBtn.addEventListener("click", startQuiz);
+//After clicking start quiz a series of questions is displayed
+function startQuiz() {
+  intro.style.display = "none";
+  renderQuestion();
+  quizEl.style.display = "block";
+}
+//Check Answers
+function checkAnsChoice(answer) {
+  if (answer == questions[currentQ].correct) {
+    answerIsCorrect();
+  } else {
+    answerIsIncorrect();
+  }
+
+  if (currentQ < lastQ) {
+    currentQ++;
+    renderQuestion();
+  } else {
+    clearInterval(setTimer);
+  }
+}
+function answerIsCorrect() {
+  document.currentQ;
+}
+function answerIsIncorrect() {
+  document.currentQ;
+}
